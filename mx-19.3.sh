efda28868e7f661b0bb3c98e78d78c0b0bd253d3
Miłosz Kłosowicz <register@miklobit.com>
2021-03-10 00:11:58 +0100

    Update mx-19.3.sh

--- mx-19.3.sh
+++ mx-19.3.sh
@@ -1,1 +1,11 @@
-
+#!/bin/sh
+# custom mx linux 19.3 configuration
+add-apt-repository $1 ppa:stefansundin/truecrypt
+add-apt-repository $1 ppa:sebastian-stenzel/cryptomator
+add-apt-repository $1 ppa:inkscape.dev/stable
+apt-get update $1
+apt-get upgrade $1
+apt-get install $1 telegram-desktop
+apt-get install $1 veracrypt
+apt-get install $1 truecrypt
+apt-get install $1 flameshot
