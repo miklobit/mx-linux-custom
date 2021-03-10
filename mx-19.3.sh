f1b518040066d79388b0296fddd8e8bd3c1f63fe
Miłosz Kłosowicz <register@miklobit.com>
2021-03-10 01:25:34 +0100

    Update mx-19.3.sh

--- mx-19.3.sh
+++ mx-19.3.sh
@@ -4,9 +4,11 @@ apt-get install software-properties-common
 apt-get update $1
 echo 'deb http://ppa.launchpad.net/stefansundin/truecrypt/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/truecrypt.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xFFE1FFFFAFEC55BB
-add-apt-repository $1 ppa:sebastian-stenzel/cryptomator
+echo 'deb http://ppa.launchpad.net/atareao/telegram/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/telegram.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A3D8A366869FE2DC5FFD79C36A9653F936FD5529
+echo 'deb http://ppa.launchpad.net/sebastian-stenzel/cryptomator/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/cryptomator.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CD045438B0D383A4039EA86F892C15CD130FEB9A
 add-apt-repository $1 ppa:inkscape.dev/stable
-add-apt-repository $1 ppa:atareao/telegram
 apt-get update $1
 apt-get upgrade $1
 apt-get install $1 net-tools
