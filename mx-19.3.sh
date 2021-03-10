fab6909308f433d947ac84ff170cdbbf4d8bce92
Miłosz Kłosowicz <register@miklobit.com>
2021-03-10 01:31:21 +0100

    Update mx-19.3.sh

--- mx-19.3.sh
+++ mx-19.3.sh
@@ -8,7 +8,8 @@ echo 'deb http://ppa.launchpad.net/atareao/telegram/ubuntu xenial main' | sudo t
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A3D8A366869FE2DC5FFD79C36A9653F936FD5529
 echo 'deb http://ppa.launchpad.net/sebastian-stenzel/cryptomator/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/cryptomator.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CD045438B0D383A4039EA86F892C15CD130FEB9A
-add-apt-repository $1 ppa:inkscape.dev/stable
+echo 'deb http://ppa.launchpad.net/inkscape.dev/stable/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/inkscape.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 22C4850146603F3DB0ED00479DA4BD18B9A06DE3
 apt-get update $1
 apt-get upgrade $1
 apt-get install $1 net-tools
@@ -16,3 +17,5 @@ apt-get install $1 telegram
 apt-get install $1 veracrypt
 apt-get install $1 truecrypt
 apt-get install $1 flameshot
+apt-get install $1 inkscape
+apt-get install $1 virtualbox
