9a3c7872f5f18e6dc3c114bd5154bbcdba704f5c
Miłosz Kłosowicz <register@miklobit.com>
2021-03-10 01:51:03 +0100

    Update mx-19.3.sh

--- mx-19.3.sh
+++ mx-19.3.sh
@@ -8,8 +8,8 @@ echo 'deb http://ppa.launchpad.net/atareao/telegram/ubuntu xenial main' | sudo t
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A3D8A366869FE2DC5FFD79C36A9653F936FD5529
 echo 'deb http://ppa.launchpad.net/sebastian-stenzel/cryptomator/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/cryptomator.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CD045438B0D383A4039EA86F892C15CD130FEB9A
-echo 'deb http://ppa.launchpad.net/inkscape.dev/stable/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/inkscape.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 22C4850146603F3DB0ED00479DA4BD18B9A06DE3
+#echo 'deb http://ppa.launchpad.net/inkscape.dev/stable/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/inkscape.list
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 22C4850146603F3DB0ED00479DA4BD18B9A06DE3
 apt-get update $1
 apt-get upgrade $1
 apt-get install $1 net-tools
@@ -18,5 +18,7 @@ apt-get install $1 veracrypt
 apt-get install $1 truecrypt
 apt-get install $1 cryptomator
 apt-get install $1 flameshot
-apt-get install $1 inkscape
+#apt-get install $1 inkscape
+apt-get install snap
+snap install inkscape
 apt-get install $1 virtualbox
